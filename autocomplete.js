d9f2ca5f476c9831fc96473680feb1b88255f658
Hangs Breaker <hangsbreaker@gmail.com>
2022-03-12 10:12:14 +0700

    Add files via upload

--- autocomplete.js
+++ autocomplete.js
@@ -11,11 +11,8 @@ function autocomplete(inpt, arr, inptres = null) {
 
   var wrapauto = document.getElementById("wrapauto-" + inpt);
 
-  /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   var ncofus = 0;
-  /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function (e) {
     let me = this;
     if ("type" in arr && "url" in arr) {
@@ -32,16 +29,12 @@ function autocomplete(inpt, arr, inptres = null) {
       loadinput(this, arr);
     }
   });
-  /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function (e) {
     var x = document.getElementById(this.id + "autocomplete-list");
     var dvl = x;
     if (x) x = x.getElementsByTagName("div");
     if (e.keyCode == 40) {
-      /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
       currentFocus++;
-      /*and and make the current item more visible:*/
       addActive(x);
 
       if (currentFocus > 7) {
@@ -53,7 +46,6 @@ function autocomplete(inpt, arr, inptres = null) {
         dvl.scrollTop = 0;
       }
     } else if (e.keyCode == 38) {
-      //up
       currentFocus--;
       addActive(x);
 
@@ -75,18 +67,18 @@ function autocomplete(inpt, arr, inptres = null) {
   function getfromsrv(me, arr) {
     let type = arr.type.toLowerCase();
     let url = type == "get" ? arr.url + "?keyword=" + me.value : arr.url;
-    $.ajax({
-      type: type,
-      url: url,
-      data: "keyword=" + me.value,
-      beforeSend: function () {
-        wrapauto.innerHTML = '<div class="autoloading">Loading</div>';
-      },
-      success: function (data) {
-        data = JSON.parse(data);
+    wrapauto.innerHTML = '<div class="autoloading">Loading...</div>';
+
+    var http = new XMLHttpRequest();
+    http.open(type, url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.onreadystatechange = function () {
+      if (http.readyState == 4 && http.status === 200) {
+        let data = JSON.parse(http.responseText);
         loadinput(me, data);
-      },
-    });
+      }
+    };
+    http.send("keyword=" + me.value);
   }
   function addActive(x) {
     if (!x) return false;
@@ -114,47 +106,39 @@ function autocomplete(inpt, arr, inptres = null) {
       b,
       i,
       val = me.value;
-    /*close any already open lists of autocompleted values*/
     closeAllLists();
-    //if (!val) { return false;}
     currentFocus = -1;
-    /*create a DIV element that will contain the items (values):*/
     a = document.createElement("DIV");
     a.setAttribute("id", me.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
-    /*append the DIV element as a child of the autocomplete container:*/
     wrapauto.appendChild(a);
-    /*for each item in the array...*/
     for (i = 0; i < arr.length; i++) {
-      /*check if the item starts with the same letters as the text field value:*/
       if (arr[i][1] !== undefined) {
         let key = val.toLowerCase();
         let text = arr[i][1].toLowerCase();
         let fnd = text.indexOf(key);
-        let ft = arr[i][1].substr(0, fnd);
-        let tt = "<strong>" + arr[i][1].substr(fnd, key.length) + "</strong>";
-        let lt = arr[i][1].substr(fnd + key.length, arr[i][1].length);
-        let words = ft + tt + lt;
+        let t = arr[i][1].substr(fnd, key.length);
+        let words =
+          arr[i][1].substr(0, fnd) +
+          "<strong>" +
+          t +
+          "</strong>" +
+          arr[i][1].substr(fnd + key.length, arr[i][1].length);
 
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = words;
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i][0] + "'>";
-        b.innerHTML += "<input type='hidden' value='" + arr[i][1] + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[1].value;
-          if (inpres != null) {
-            inpres.value = this.getElementsByTagName("input")[0].value;
-          }
-          /*close the list of autocompleted values,
-                    (or any other open lists of autocompleted values:*/
-          closeAllLists();
-        });
-        a.appendChild(b);
+        if (t.toLowerCase() == key) {
+          b = document.createElement("DIV");
+          b.innerHTML = words;
+          b.innerHTML += "<input type='hidden' value='" + arr[i][0] + "'>";
+          b.innerHTML += "<input type='hidden' value='" + arr[i][1] + "'>";
+          b.addEventListener("click", function (e) {
+            inp.value = this.getElementsByTagName("input")[1].value;
+            if (inpres != null) {
+              inpres.value = this.getElementsByTagName("input")[0].value;
+            }
+            closeAllLists();
+          });
+          a.appendChild(b);
+        }
       }
     }
   }
